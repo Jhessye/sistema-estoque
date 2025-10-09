93986b50d650acb7f4c77d472ac5a680bc7a3eae
Jhessye Lorrayne <122307033+Jhessye@users.noreply.github.com>
2025-10-09 16:47:37 -0300

    Fix SQL case sensitivity in column aliases

--- src/main/java/sql/relatorioMovimentacaoPorProduto.sql
+++ src/main/java/sql/relatorioMovimentacaoPorProduto.sql
@@ -1,5 +1,5 @@
-SELECT p.Nome AS produto,
-       SUM(m.Valor) AS TotalMovimentado
+SELECT p.nome AS Produto,
+       SUM(m.valor) AS TotalMovimentado
 FROM movimentacao m
 JOIN produto p ON p.id_produto = m.id_produto
 GROUP BY p.nome;
