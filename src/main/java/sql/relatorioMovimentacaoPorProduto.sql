f3a4fcfb1951033b99bcc2a3440c22368f6359ee
Jhessye Lorrayne <122307033+Jhessye@users.noreply.github.com>
2025-10-22 21:09:58 -0300

    atualização dos sql

--- src/main/java/sql/relatorioMovimentacaoPorProduto.sql
+++ src/main/java/sql/relatorioMovimentacaoPorProduto.sql
@@ -1,5 +1,5 @@
 SELECT p.nome AS Produto,
        SUM(m.valor) AS TotalMovimentado
 FROM movimentacoes m
-JOIN produto p ON p.id_produto = m.id_produto
+JOIN produtos p ON p.id_produto = m.id_produto
 GROUP BY p.nome;
--- target/classes/sql/relatorioMovimentacoesCategoriaProduto.sql
+++ target/classes/sql/relatorioMovimentacoesCategoriaProduto.sql
@@ -2,11 +2,11 @@ SELECT
     m.id_movimentacoes AS idMovimentacoes,
     m.data,
     p.id_produto AS idProduto,
-    p.nome AS produto,
+    p.nome AS produtos,
     p.quantidade,
     p.preco AS valor,
     c.id_categoria AS idCategoria,
     c.nome AS categoria
 FROM movimentacoes m
-JOIN produto p ON p.id_produto = m.id_produto
-JOIN categoria c ON c.id_categoria = p.id_categoria;
+JOIN produtos p ON p.id_produto = m.id_produto
+JOIN categorias c ON c.id_categoria = p.id_categoria;
